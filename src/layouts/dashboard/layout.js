c427f1698439cdfa1b3d2329a65766f180b71987
Branislav Damjanovic <branislav_damjanovic@flutterint.com>
2024-08-06 09:26:37 +0200

    mozda je proradilo :D

--- src/layouts/dashboard/layout.js
+++ src/layouts/dashboard/layout.js
@@ -8,55 +8,57 @@ import { TopNav } from './top-nav';
 const SIDE_NAV_WIDTH = 280;
 
 const LayoutRoot = styled('div')(({ theme }) => ({
-          display: 'flex',
-          flex: '1 1 auto',
-          maxWidth: '100%',
-          [theme.breakpoints.up('lg')]: {
-                    paddingLeft: SIDE_NAV_WIDTH
-          }
+     display: 'flex',
+     flex: '1 1 auto',
+     maxWidth: '100%',
+     [theme.breakpoints.up('lg')]: {
+          paddingLeft: SIDE_NAV_WIDTH
+     }
 }));
 
 const LayoutContainer = styled('div')({
-          display: 'flex',
-          flex: '1 1 auto',
-          flexDirection: 'column',
-          width: '100%'
+     display: 'flex',
+     flex: '1 1 auto',
+     flexDirection: 'column',
+     width: '100%'
 });
 
 export const Layout = withAuthGuard((props) => {
-          const { children } = props;
-          const pathname = usePathname();
-          const [openNav, setOpenNav] = useState(false);
+     console.log('Layout', props);
 
-          const handlePathnameChange = useCallback(
-                    () => {
-                              if (openNav) {
-                                        setOpenNav(false);
-                              }
-                    },
-                    [openNav]
-          );
+     const { children } = props;
+     const pathname = usePathname();
+     const [openNav, setOpenNav] = useState(false);
 
-          useEffect(
-                    () => {
-                              handlePathnameChange();
-                    },
-                    // eslint-disable-next-line react-hooks/exhaustive-deps
-                    [pathname]
-          );
+     const handlePathnameChange = useCallback(
+          () => {
+               if (openNav) {
+                    setOpenNav(false);
+               }
+          },
+          [openNav]
+     );
 
-          return (
-                    <>
-                              <TopNav onNavOpen={() => setOpenNav(true)} />
-                              <SideNav
-                                        onClose={() => setOpenNav(false)}
-                                        open={openNav}
-                              />
-                              <LayoutRoot>
-                                        <LayoutContainer>
-                                                  {children}
-                                        </LayoutContainer>
-                              </LayoutRoot>
-                    </>
-          );
+     useEffect(
+          () => {
+               handlePathnameChange();
+          },
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          [pathname]
+     );
+
+     return (
+          <>
+               <TopNav onNavOpen={() => setOpenNav(true)} />
+               <SideNav
+                    onClose={() => setOpenNav(false)}
+                    open={openNav}
+               />
+               <LayoutRoot>
+                    <LayoutContainer>
+                         {children}
+                    </LayoutContainer>
+               </LayoutRoot>
+          </>
+     );
 });
